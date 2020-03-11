20eb0629dc259123d47623d29b484cc58832e773
Kiran Johns <kiranjohns369@gmail.com>
2020-03-11 19:21:48 +0530

    datatable

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import dash
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
 import pandas as pd
@@ -18,80 +19,26 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 df = pd.read_csv(COVID)
 
 app.layout = html.Div([
-    dcc.Graph(id='death-tolls'),
-    dcc.Slider(
-        id='date-slider',
-        min = df['ObservationDate'].min(),
-        max = df['ObservationDate'].max(),
-        value = df['ObservationDate'].min(),
-        marks={str(date): str(date) for date in df['ObservationDate'].unique()},
-        step = None
-    )
+    dash_table.DataTable(
+        id = 'covid-table',
+        columns = [
+            {"name":i,"id":i,"selectable":True} for i in df.columns
+        ],
+        data=df.to_dict('records'),
+        editable=True,
+        filter_action="native",
+        sort_action="native",
+        sort_mode="multi",
+        column_selectable="single",
+        row_selectable="multi",
+        selected_columns=[],
+        selected_rows=[],
+        page_action="native",
+        page_current= 0,
+        page_size= 10,
+    ),
+    html.Div(id='datatable-interactivity-container')
 ])
-
-@app.callback(
-    Output('death-tolls','figure'),
-    [Input('date-slider','value')]
-)
-def update_figure(selected_date):
-    filtered_df = df[df.year == selected_date]
-    traces = []
-    for i in filtered_df['Country/Region'] .unique():
-        df_by_region = filtered_df[filtered_df['Country/Region']  == i]
-        traces.append(dict(
-            x=df[df['Country/Region'] == i]['ObservationDate'],
-            y=df[df['Country/Region'] == i]['Deaths'],
-            mode='markers',
-            opacity=0.7,
-            marker={
-            'size': 15,
-            'line': {'width': 0.5, 'color': 'white'}
-            },
-            name=i
-        ))
-
-    return {
-        'data': traces,
-        'layout': dict(
-            xaxis={'type': 'log', 'title': 'GDP Per Capita',
-                   'range':[2.3, 4.8]},
-            yaxis={'title': 'Life Expectancy', 'range': [20, 90]},
-            margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-            legend={'x': 0, 'y': 1},
-            hovermode='closest',
-            transition = {'duration': 500},
-        )
-    }
-
-'''
-app.layout = html.Div([
-    dcc.Graph(
-        id='COVID-19',
-        figure={
-            'data': [
-                dict(
-                    x=df[df['Country/Region'] == i]['ObservationDate'],
-                    y=df[df['Country/Region'] == i]['Deaths'],
-                    mode='markers',
-                    opacity=0.7,
-                    marker={
-                        'size': 15,
-                        'line': {'width': 0.5, 'color': 'white'}
-                    },
-                    name=i
-                ) for i in df['Country/Region'].unique() 
-            ],
-            'layout': dict(
-                xaxis={'title': 'Date'},
-                yaxis={'title': 'Deaths'},
-                margin={'l': 40, 'b': 40, 't': 10, 'r': 10},
-                legend={'x': 0, 'y': 1},
-                hovermode='closest'
-            )
-        }
-    )
-])
-'''
 
 if __name__ == '__main__':
     app.run_server(debug=True)
